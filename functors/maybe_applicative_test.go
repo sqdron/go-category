e0c22cc939e4e7d729e10a2491d90da28fcc393d
Denis Korkhov <sqdron.io@gmail.com>
2016-07-14 11:44:01 +0300

    refactoring

--- functors/maybe_applicative_test.go
+++ functors/maybe_applicative_test.go
@@ -4,39 +4,60 @@ import (
 	"testing"
 	. "github.com/smartystreets/goconvey/convey"
 	"github.com/smartystreets/assertions/should"
+	"github.com/sqdron/go-category"
 )
 
-func Test_Maybe_Call(t *testing.T) {
-	Convey("Just call Maybe func", t, func() {
-		result := Maybe(5);
-		j := result.(Just);
-		So(j.value, should.Equal, 5);
+func Test_Maybe_Applicative_Sum(t *testing.T) {
+	Convey("Just call Maybe with sum applicative", t, func() {
+		var add category.Morphism = func(value interface{}) interface{} {
+			var result category.Morphism = func(value2 interface{}) interface{} {
+				return value.(int) + value2.(int);
+			}
+			return result;
+		}
+
+		result := Maybe(add).A(J(4)).A(J(2));
+		So(result.(Just).value, should.Equal, 6);
 	});
 }
 
-func Test_Maybe_Squence_Double(t *testing.T) {
-	Convey("Call maybe an apply morphism", t, func() {
-		doubleMe := func(i interface{}) interface{} {
-			return 2 * i.(int)
-		};
+func Test_Maybe_Applicative_Add_3(t *testing.T) {
+	Convey("Just call Maybe with +3 applicative", t, func() {
+		var add category.Morphism = func(value interface{}) interface{} {
+			return value.(int) + 3;
+		}
 
-		result := Maybe(5).Fmap(doubleMe).Fmap(doubleMe);
-		j := result.(Just);
-		So(j.value, should.Equal, 20);
+		result := Maybe(add).A(J(4));
+		So(result.(Just).value, should.Equal, 7);
 	});
 }
 
-func Test_Maybe_Squence_Double_And_Nothing(t *testing.T) {
-	Convey("Call maybe an apply morphism and one morphism wich return Nil", t, func() {
-		doubleMe := func(i interface{}) interface{} {
-			return 2 * i.(int)
-		};
+func Test_Maybe_Applicative_Multiplicate(t *testing.T) {
+	Convey("Just call Maybe with multiplication applicative", t, func() {
+		var mult category.Morphism = func(value interface{}) interface{} {
+			var result category.Morphism = func(value2 interface{}) interface{} {
+				return value.(int) * value2.(int);
+			}
+			return result;
+		}
 
-		nothingBack := func(i interface{}) interface{} {
-			return nil;
-		};
+		result := Maybe(mult).A(J(4)).A(J(3));
+		So(result.(Just).value, should.Equal, 12);
+	});
+}
 
-		result := Maybe(5).Fmap(nothingBack).Fmap(doubleMe);
+
+func Test_Maybe_Applicative_Multiplicate_With_Nothing(t *testing.T) {
+	Convey("Just call Maybe with multiplication applicative and nothing", t, func() {
+		var mult category.Morphism = func(value interface{}) interface{} {
+			var result category.Morphism = func(value2 interface{}) interface{} {
+				return value.(int) * value2.(int);
+			}
+			return result;
+		}
+
+		result := Maybe(mult).A(J(4)).A(N());
 		So(result, should.Resemble, Nothing{});
 	});
 }
+
