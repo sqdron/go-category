d0590fe5f19772c47c360b7ff19bf76cba1e6a6e
Denis Korkhov <sqdron.io@gmail.com>
2016-08-05 15:30:09 +0300

    golint

--- README.md
+++ README.md
@@ -3,6 +3,7 @@
 ==============================
 
 [![Build Status](https://travis-ci.org/sqdron/go-category.svg?branch=master)](https://travis-ci.org/sqdron/go-category?branch=master)
+[![Go Report Card](https://goreportcard.com/badge/github.com/sqdron/go-category)](https://goreportcard.com/report/github.com/sqdron/go-category)
 
 **Features:**
 
--- theory/list.go
+++ theory/list.go
@@ -2,10 +2,12 @@ package theory
 
 type list []interface{}
 
+//IList is an interface for a List Monad
 type IList interface {
 	IMonad
 }
 
+//List constructor
 func List(slice ...interface{}) Category {
 	if slice == nil {
 
@@ -15,10 +17,12 @@ func List(slice ...interface{}) Category {
 	return Category{context: lst}
 }
 
+//FromList is a list constructor
 func FromList(lst list) Category {
 	return Category{context: lst}
 }
 
+//Map is a Map implementation for a list
 func (val list) Map(f Morphism) Category {
 	var result list = []interface{}{}
 	for _, v := range val {
@@ -27,24 +31,25 @@ func (val list) Map(f Morphism) Category {
 	return FromList(result)
 }
 
+//Map is an Applicative implementation for a list
 func (val list) Applicative(jm Category) Category {
 	var appl = jm.Value()
 	if (appl == empty{}) {
 		return Nothing()
 	}
-	var f Morphism = appl.(Morphism)
 	var result list = []interface{}{}
 	for _, v := range val {
-		result = append(result, f(v))
+		result = append(result, appl.(Morphism)(v))
 	}
 	return FromList(result)
 }
 
-// fmap f Nothing = Nothing
+//Bind - fmap f Nothing = Nothing
 func (val list) Bind(f func(interface{}) Category) Category {
 	return f(val)
 }
 
+//Value extract a value
 func (val list) Value() interface{} {
 	return val
 }
