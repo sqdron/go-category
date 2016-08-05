9712eec919b91d71d5969626e00e045662462c14
Denis Korkhov <sqdron.io@gmail.com>
2016-08-05 15:10:07 +0300

    go fmt

--- helpers/InterfaceSlice.go
+++ helpers/InterfaceSlice.go
@@ -1,8 +1,8 @@
-package helpers;
+package helpers
 
 import "reflect"
 
-func InterfaceSlice(slice interface{}) [] interface{} {
+func InterfaceSlice(slice interface{}) []interface{} {
 	s := reflect.ValueOf(slice)
 	if s.Kind() != reflect.Slice {
 		return []interface{}{slice}
