d149ef87795084bd993441308e63629d60eae6bb
Denis Korkhov <sqdron.io@gmail.com>
2016-08-05 14:55:11 +0300

    go fmt

--- theory/list.go
+++ theory/list.go
@@ -1,50 +1,50 @@
 package theory
 
-type list []interface{};
+type list []interface{}
 
 type IList interface {
 	IMonad
 }
 
 func List(slice ...interface{}) Category {
-	if (slice == nil){
+	if slice == nil {
 
-		return Category{context:list{}}
-	};
-	var lst list = slice;
-	return Category{context:lst};
+		return Category{context: list{}}
+	}
+	var lst list = slice
+	return Category{context: lst}
 }
 
 func FromList(lst list) Category {
-	return Category{context:lst};
+	return Category{context: lst}
 }
 
-func (val list) Map(f Morphism) Category{
-	var result list = []interface{}{};
+func (val list) Map(f Morphism) Category {
+	var result list = []interface{}{}
 	for _, v := range val {
 		result = append(result, f(v))
 	}
-	return FromList(result);
+	return FromList(result)
 }
 
-func (val list) Applicative(jm Category) Category{
-	var appl = jm.Value();
+func (val list) Applicative(jm Category) Category {
+	var appl = jm.Value()
 	if (appl == empty{}) {
 		return Nothing()
 	}
-	var f Morphism = appl.(Morphism);
-	var result list = []interface{}{};
+	var f Morphism = appl.(Morphism)
+	var result list = []interface{}{}
 	for _, v := range val {
 		result = append(result, f(v))
 	}
-	return FromList(result);
+	return FromList(result)
 }
 
 // fmap f Nothing = Nothing
-func (val list) Bind(f func(interface{}) Category) Category{
-	return f(val);
+func (val list) Bind(f func(interface{}) Category) Category {
+	return f(val)
 }
 
-func (val list) Value() interface {}{
-	return val;
+func (val list) Value() interface{} {
+	return val
 }
