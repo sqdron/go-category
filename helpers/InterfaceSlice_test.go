cbe1393d5b444357abfad868857e82633c070340
Denis Korkhov <sqdron.io@gmail.com>
2016-08-27 15:56:18 +0300

    Tests for helpers module

--- helpers/InterfaceSlice_test.go
+++ helpers/InterfaceSlice_test.go
@@ -1,1 +1,15 @@
 package helpers
+
+import (
+	. "github.com/smartystreets/goconvey/convey"
+	"testing"
+)
+
+func Test_Slices(t *testing.T) {
+	var m1 interface{} = []int{1, 2, 3}
+
+	Convey("Make slice of interfaces", t, func() {
+		mResult := InterfaceSlice(m1)
+		So(len(mResult), ShouldEqual, 3)
+	})
+}
